693addeef7507482576d7b2256ee70d01af6fb6f
smy-101 <824860484@qq.com>
2023-04-21 10:43:54 +0800

    feature:update useInterval

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,11 +1,12 @@
-import { useEffect, useRef } from 'react'
+import {useCallback, useEffect, useRef} from 'react';
 
 export function useInterval(callback: () => void, delay: number | null) {
-  const savedCallback = useRef(callback)
+  const callbackRef = useRef(callback);
+  const intervalRef = useRef<number>();
 
   //记录回调函数
   useEffect(() => {
-    savedCallback.current = callback
+    callbackRef.current = callback
   }, [callback])
 
   //设置interval
@@ -14,9 +15,22 @@ export function useInterval(callback: () => void, delay: number | null) {
       return
     }
 
-    const id = setInterval(() => savedCallback.current(), delay)
+    intervalRef.current = setInterval(() => callbackRef.current(), delay)
 
-    return () => clearInterval(id)
-  }, [delay])
+    return () => clearInterval(intervalRef.current)
+  }, [delay]);
+
+  // 返回一个清除定时器的函数
+  const clear = useCallback(() => {
+    clearTimeout(intervalRef.current);
+  }, []);
+
+  //重置定时器
+  const reset = useCallback(() => {
+    clear();
+    intervalRef.current = setTimeout(() => callbackRef.current(), delay!);
+  }, [delay, clear]);
+
+  return {reset,clear}
 }
 
--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -19,12 +19,12 @@ export function useTimeout(callback: () => void, delay: number | null) {
     timeoutRef.current = setTimeout(() => callbackRef.current(), delay);
 
     //组件卸载和变化时移除定时器
-    return () => clearTimeout(timeoutRef.current!);
+    return () => clearTimeout(timeoutRef.current);
   }, [delay]);
 
   // 返回一个清除定时器的函数
   const clear = useCallback(() => {
-    clearTimeout(timeoutRef.current!);
+    clearTimeout(timeoutRef.current);
   }, []);
 
   //重置定时器
